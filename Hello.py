e936782da4a5cb8e10350d18903f5d66c09d8f6a
aditimaheshwari <aditimaheshwari1999@gmail.com>
2024-03-09 13:59:02 +0000

    Updated

--- Hello.py
+++ Hello.py
@@ -15,6 +15,20 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from scipy.special import inv_boxcox
+from scipy.stats import boxcox_normplot
+
 LOGGER = get_logger(__name__)
 
 
